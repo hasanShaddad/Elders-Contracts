87183f9fa41b91ebcd25d9b656e7d5e00b039e4e
HasanShaddad <egyptioncoder@gmail.com>
2019-07-28 22:45:09 +0200

    edit EldersVoteManag private to internal for _contractVoteDetails

--- Contracts/EldersVotingManag.sol
+++ Contracts/EldersVotingManag.sol
@@ -55,7 +55,7 @@ contract EldersVotingManag
    uint AgrredVoicesCount;
     }
         
-    ContractVoteDetails private  _ContractVoteDetails;
+    ContractVoteDetails internal  _ContractVoteDetails;
     ElderVoteDetails private _ElderVoteDetails;
     
     
