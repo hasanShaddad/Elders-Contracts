e7ef4ba67b5311dc9c1c5bdd8acad42c455d3aac
HasanShaddad <egyptioncoder@gmail.com>
2019-07-30 11:55:00 +0200

    update the temp test contract adding  require to  setMaxRollArrayVal

--- Contracts/Test.sol
+++ Contracts/Test.sol
@@ -7,19 +7,39 @@ pragma solidity ^ 0.5.1;
 */
 
 import "./EldersRoles.sol";
-
-contract test {
+import "./EldersLogicManag.sol";
+import "./EldersVotingManag.sol";
+contract test is EldersVotingManag,EldersLogicManag {
+    
+    
+     constructor(
+         address[] memory  eldersAddresses,
+        uint  minimumEldersPercentageToVote 
+        
+    ) public   EldersLogicManag(
+        eldersAddresses,
+          minimumEldersPercentageToVote 
+       
+    ){
+        
+        _owner = msg.sender; 
+    }
+    
+    
      using EldersRoles for EldersRoles.Roles;
      
         EldersRoles.Roles private _roles;
-        
-        
+      
+       
         function addRoleToAccount(address _account ,uint _role )public{
             _roles.AddRoleToAccount(_role,_account);
         }
         
-        function setMaxRollArrayVal(uint8 val)public{
+        function setMaxRollArrayVal(uint8 val)internal{
+            require( LogicContractIsValid(msg.sender,1),"sender has no access to this func");
               _roles.SetMaxRolesArrayLength(val);
         }
         
+        
+      
 }
